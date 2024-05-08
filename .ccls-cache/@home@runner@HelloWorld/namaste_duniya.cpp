529e43ff2a4dbddbb673de9b66b2c1dbfa987253
saurav97531 <33897014-saurav97531@users.noreply.replit.com>
2024-05-08 07:22:40 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@HelloWorld/namaste_duniya.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int namaste() {
+  cout << "Namaste World!\n";
+  return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int namaste();
+int addTwoNumber();
+
+int main() {
+  namaste();
+  addTwoNumber();
+}
